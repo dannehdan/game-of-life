f5df15c119c1f65b7a9176bcb9f1f4e0d49d7844
Dan Marshall <daniel.o.marshall@gmail.com>
2021-09-17 14:14:04 +0100

    Set random board

--- lib/game_of_life.rb
+++ lib/game_of_life.rb
@@ -1,10 +1,12 @@
-def initialize(n, val)
+def set_board(n)
   board = []
   n.times do |row_index|
     board[row_index] = [n]
     n.times do |column_index|
-      board[row_index][column_index] = val
+      board[row_index][column_index] = rand(2)
     end
   end
-  board
+  print board
 end
+
+set_board(4)
--- spec/game_of_life_spec.rb
+++ spec/game_of_life_spec.rb
@@ -2,6 +2,6 @@ require './lib/game_of_life.rb'
 
 describe 'Game of Life' do
   it 'returns "Hello" when run' do
-    expect(initialize(3,1)).to eq([1, 1, 1], [1, 1, 1], [1, 1, 1])
+    expect(set_board(3)).to eq([1, 1, 1], [1, 1, 1], [1, 1, 1])
   end
 end
