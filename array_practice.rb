327208b25a0c3153d06460767d68718a8f7e6d34
Dan Marshall <daniel.o.marshall@gmail.com>
2021-09-20 10:21:35 +0100

    Changed state of cell based on neighbours

--- array_practice.rb
+++ array_practice.rb
@@ -1,17 +1,20 @@
-arr = [[1, 1, 1], [1, 0, 1], [1, 1, 1]]
-arr.each do |x|
-  print "#{x}\n"
+@arr = [[1, 1, 1], [1, 1, 1], [1, 1, 1]]
+def print_array
+  @arr.each do |x|
+    print "#{x}\n"
+  end
 end
 
-# print arr [1][1]
+print_array
 
 x = 0
 y = 0
 
-puts arr[x][y]
+puts @arr[x][y]
+
 
 total = []
-arr.each do |thing|
+@arr.each do |thing|
   y = 0
   thing.each do |other_thing|
     total.push(thing[y])
@@ -19,4 +22,10 @@ arr.each do |thing|
   end
 end
 
-print total.sum
+ if total.sum > 3
+  @arr[1][1] = 0
+ else
+  @arr[1][1] = 1
+end
+
+print_array
